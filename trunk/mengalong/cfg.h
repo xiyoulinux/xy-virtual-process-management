df795930cb474cb318e51186479c2c910adcbd5c
boyan1105 <boyan1105@75493c20-c87d-11de-a380-73d83ba7c083>
2009-11-06 04:24:09 +0000

    新版本09/11/06

--- trunk/mengalong/cfg.h
+++ trunk/mengalong/cfg.h
@@ -1,1 +1,3 @@
-#define PROCESS_MAX_NUM		255
+/*定义的是相关常量*/
+#define PROCESS_MAX_NUM		255		/*系统最多创建的进程数*/
+#define STAT_RDY			0		/*进程的状态*/
--- trunk/mengalong/main.c
+++ trunk/mengalong/main.c
@@ -1,20 +1,18 @@
+/*主程序*/
 #include "includes.h"
 
 int main()
 {
 	char ch = 'y';
-	unsigned short  pid;
-//	PCB pcb;
-	struct task_struct pcb;
+	struct task_struct *pcb;
 
 	while(ch != 'q') {
 		printf(">");	
 		ch = getchar();
 		if('n' == ch) {
 			getchar();
-			
 			pcb = create_process();
-			printf(">create process sucess! %d\n", pcb.PCBpid);
+			printf(">create process sucess! %d\n", pcb->PCBpid);
 			
 		} else {
 		}
